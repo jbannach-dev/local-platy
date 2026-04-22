21353f0b53d7a5c9398fb19647d07f44ca1d4ea8
jbannach-dev <jacek.bannach@outlook.de>
2026-04-22 17:28:03 +0200

    style: auto-format code

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -21,14 +21,8 @@ use tokio::sync::{mpsc, oneshot};
 
 use model::{spawn_thread, ModelState, ModelTask};
 
-
-
 #[tauri::command]
-async fn prompt(
-    text: String,
-    state: tauri::State<'_, ModelState>,
-) -> Result<String, String>{
-    
+async fn prompt(text: String, state: tauri::State<'_, ModelState>) -> Result<String, String> {
     let (res_tx, res_rx) = oneshot::channel();
 
     state
@@ -47,21 +41,17 @@ async fn prompt(
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .setup(|app|{
+        .setup(|app| {
             let model_path = app
                 .path()
-                .resolve(
-                    "models/model.gguf",
-                    BaseDirectory::Resource,
-                )
+                .resolve("models/model.gguf", BaseDirectory::Resource)
                 .expect("Failed to find the model");
 
-                let context_size = 1024;
-                let system_prompt = "You are an Assistant".to_string();
+            let context_size = 1024;
+            let system_prompt = "You are an Assistant".to_string();
 
-                let tx = model::spawn_thread(model_path, context_size, system_prompt);
-                app.manage(ModelState{tx});
-
+            let tx = model::spawn_thread(model_path, context_size, system_prompt);
+            app.manage(ModelState { tx });
 
             Ok(())
         })
