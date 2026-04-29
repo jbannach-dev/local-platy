a3e6f03f87982dc9fd39a200ddd8121dc4c8d245
jbannach-dev <jacek.bannach@outlook.de>
2026-04-29 23:57:15 +0200

    refactor(model): implement Execute pattern for extensible command handling

--- src-tauri/src/model.rs
+++ src-tauri/src/model.rs
@@ -20,6 +20,7 @@ use tauri::Manager;
 use tauri_plugin_fs::FsExt;
 
 use llama_cpp_2::context::params::LlamaContextParams;
+use llama_cpp_2::context::LlamaContext;
 use llama_cpp_2::llama_backend::LlamaBackend;
 use llama_cpp_2::llama_batch::LlamaBatch;
 use llama_cpp_2::model::params::LlamaModelParams;
@@ -32,13 +33,150 @@ use encoding_rs::UTF_8;
 
 use tokio::sync::{mpsc, oneshot};
 
-pub struct ModelTask {
-    pub text: String,
-    pub response_tx: oneshot::Sender<String>,
+pub enum ModelCommands {
+    Prompt {
+        text: String,
+        response_tx: oneshot::Sender<String>,
+    },
 }
 
 pub struct ModelState {
-    pub tx: mpsc::Sender<ModelTask>,
+    pub tx: mpsc::Sender<ModelCommands>,
+}
+
+impl ModelCommands {
+    fn exec(
+        self,
+        model: &LlamaModel,
+        ctx: &mut LlamaContext,
+        seq_id: u32,
+        max_seq: i32,
+        seq_pos_y: &mut i32,
+        context_size: u32,
+        system_prompt_tokens_position: i32,
+    ) {
+        match self {
+            ModelCommands::Prompt { text, response_tx } => {
+                Self::handle_prompt(
+                    model,
+                    ctx,
+                    seq_id,
+                    max_seq,
+                    seq_pos_y,
+                    context_size,
+                    system_prompt_tokens_position,
+                    text,
+                    response_tx,
+                );
+            }
+        }
+    }
+
+    //Handle prompt logic
+    fn handle_prompt(
+        model: &LlamaModel,
+        ctx: &mut LlamaContext,
+        seq_id: u32,
+        max_seq: i32,
+        seq_pos_y: &mut i32,
+        context_size: u32,
+        system_prompt_tokens_position: i32,
+        text: String,
+        response_tx: oneshot::Sender<String>,
+    ) {
+        let recived_text = text;
+        let mut prompt = String::from("");
+        prompt += "<|im_start|>user\n";
+        prompt += &recived_text;
+        prompt += "<|im_end|>\n";
+        prompt += "<|im_start|>assistant\n";
+        prompt += "<think>";
+
+        let tokens = model.str_to_token(prompt.as_str(), AddBos::Never).unwrap();
+        let mut batch = LlamaBatch::new(max_seq as usize, 1);
+        for (i, token) in tokens.iter().enumerate() {
+            let _ = batch.add(*token, *seq_pos_y as i32, &[0][..], i == tokens.len() - 1);
+            *seq_pos_y += 1;
+        }
+        let _ = ctx.decode(&mut batch);
+
+        //Sampler
+        let mut rng = rand::thread_rng();
+        let seed: u32 = rng.gen();
+
+        let mut sampler = LlamaSampler::chain_simple([
+            LlamaSampler::penalties(64, 1.1, 0.0, 2.0),
+            LlamaSampler::temp(0.5),
+            LlamaSampler::min_p(0.05, 1),
+            LlamaSampler::dist(seed),
+        ]);
+
+        let mut n_cur = tokens.len() as i32;
+        let mut output = String::from("");
+        let mut decoder = UTF_8.new_decoder();
+
+        while n_cur < max_seq {
+            //Sliding window implementation
+            let current_seq_pos = (*seq_pos_y) as u32;
+
+            if context_size < current_seq_pos {
+                // Set the deletion size to 25% of the maximum context capacity.
+                let mut delete_by = (context_size as f32 * 0.25).round() as i32;
+                let difference = (current_seq_pos - context_size) as i32;
+
+                // Include the overflowed context in the deletion range if there is any.
+                if difference > 0 {
+                    delete_by += difference;
+                }
+
+                // Set parameters for the upcoming shift
+                let tokens_to_keep = system_prompt_tokens_position as u32;
+                let tokens_to_discard = (system_prompt_tokens_position + delete_by) as u32;
+                let current_seq_position = *seq_pos_y as u32;
+                let shift_by = delete_by as i32;
+                let new_seq_position = (*seq_pos_y - delete_by) as u32;
+
+                // Clear the KV cache while preserving the system prompt.
+                ctx.clear_kv_cache_seq(Some(seq_id), Some(tokens_to_keep), Some(tokens_to_discard))
+                    .unwrap();
+
+                // Shift the remaining KV cache forward to close the gap after the system prompt.
+                ctx.kv_cache_seq_add(
+                    seq_id as i32,
+                    Some(tokens_to_discard),
+                    Some(current_seq_position),
+                    -shift_by,
+                )
+                .unwrap();
+
+                // Clear the end of the KV cache to remove duplicate context
+                ctx.clear_kv_cache_seq(Some(seq_id), Some(new_seq_position), None)
+                    .unwrap();
+
+                *seq_pos_y = (new_seq_position) as i32;
+            }
+
+            let token_id = sampler.sample(&ctx, batch.n_tokens() - 1);
+
+            if token_id == model.token_eos() {
+                break;
+            }
+
+            if let Ok(piece) = model.token_to_piece(token_id, &mut decoder, false, None) {
+                output += &piece;
+            } else {
+                eprintln!("Warning: undefined Token-Type at ID {}", token_id);
+            }
+
+            batch.clear();
+
+            let _ = batch.add(token_id, *seq_pos_y, &[0][..], true);
+            let _ = ctx.decode(&mut batch);
+            *seq_pos_y += 1;
+            n_cur += 1;
+        }
+        let _ = response_tx.send(output);
+    }
 }
 
 pub fn spawn_thread(
@@ -46,8 +184,8 @@ pub fn spawn_thread(
     model_path: PathBuf,
     context_size: u32,
     system_prompt: String,
-) -> mpsc::Sender<ModelTask> {
-    let (tx, mut rx) = mpsc::channel::<ModelTask>(10);
+) -> mpsc::Sender<ModelCommands> {
+    let (tx, mut rx) = mpsc::channel::<ModelCommands>(10);
 
     thread::spawn(move || {
         let finnal_path;
@@ -117,104 +255,15 @@ pub fn spawn_thread(
         let _ = ctx.decode(&mut system_prompt_batch);
 
         while let Some(task) = rx.blocking_recv() {
-            let recived_text = task.text;
-
-            //Handle prompt logic
-            let mut prompt = String::from("");
-            prompt += "<|im_start|>user\n";
-            prompt += &recived_text;
-            prompt += "<|im_end|>\n";
-            prompt += "<|im_start|>assistant\n";
-            prompt += "<think>";
-
-            let tokens = model.str_to_token(prompt.as_str(), AddBos::Never).unwrap();
-            let mut batch = LlamaBatch::new(max_seq as usize, 1);
-            for (i, token) in tokens.iter().enumerate() {
-                let _ = batch.add(*token, seq_pos_y as i32, &[0][..], i == tokens.len() - 1);
-                seq_pos_y += 1;
-            }
-            let _ = ctx.decode(&mut batch);
-
-            //Sampler
-            let mut rng = rand::thread_rng();
-            let seed: u32 = rng.gen();
-
-            let mut sampler = LlamaSampler::chain_simple([
-                LlamaSampler::penalties(64, 1.1, 0.0, 2.0),
-                LlamaSampler::temp(0.5),
-                LlamaSampler::min_p(0.05, 1),
-                LlamaSampler::dist(seed),
-            ]);
-
-            let mut n_cur = tokens.len() as i32;
-            let mut output = String::from("");
-            let mut decoder = UTF_8.new_decoder();
-
-            while n_cur < max_seq {
-                //Sliding window implementation
-                let current_seq_pos = (seq_pos_y) as u32;
-
-                if context_size < current_seq_pos {
-                    // Set the deletion size to 25% of the maximum context capacity.
-                    let mut delete_by = (context_size as f32 * 0.25).round() as i32;
-                    let difference = (current_seq_pos - context_size) as i32;
-
-                    // Include the overflowed context in the deletion range if there is any.
-                    if difference > 0 {
-                        delete_by += difference;
-                    }
-
-                    // Set parameters for the upcoming shift
-                    let tokens_to_keep = system_prompt_tokens_position as u32;
-                    let tokens_to_discard = (system_prompt_tokens_position + delete_by) as u32;
-                    let current_seq_position = seq_pos_y as u32;
-                    let shift_by = delete_by as i32;
-                    let new_seq_position = (seq_pos_y - delete_by) as u32;
-
-                    // Clear the KV cache while preserving the system prompt.
-                    ctx.clear_kv_cache_seq(
-                        Some(seq_id),
-                        Some(tokens_to_keep),
-                        Some(tokens_to_discard),
-                    )
-                    .unwrap();
-
-                    // Shift the remaining KV cache forward to close the gap after the system prompt.
-                    ctx.kv_cache_seq_add(
-                        seq_id as i32,
-                        Some(tokens_to_discard),
-                        Some(current_seq_position),
-                        -shift_by,
-                    )
-                    .unwrap();
-
-                    // Clear the end of the KV cache to remove duplicate context
-                    ctx.clear_kv_cache_seq(Some(seq_id), Some(new_seq_position), None)
-                        .unwrap();
-
-                    seq_pos_y = (new_seq_position) as i32;
-                }
-
-                let token_id = sampler.sample(&ctx, batch.n_tokens() - 1);
-
-                if token_id == model.token_eos() {
-                    break;
-                }
-
-                if let Ok(piece) = model.token_to_piece(token_id, &mut decoder, false, None) {
-                    output += &piece;
-                } else {
-                    eprintln!("Warning: undefined Token-Type at ID {}", token_id);
-                }
-
-                batch.clear();
-
-                let _ = batch.add(token_id, seq_pos_y, &[0][..], true);
-                let _ = ctx.decode(&mut batch);
-                seq_pos_y += 1;
-                n_cur += 1;
-            }
-            let _ = task.response_tx.send(output);
+            task.exec(
+                &model,
+                &mut ctx,
+                seq_id,
+                max_seq,
+                &mut seq_pos_y,
+                context_size,
+                system_prompt_tokens_position,
+            );
         }
     });
 
